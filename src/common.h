a8a4a0f310c96ed67db4d8fc238aa09d5d2dc4e3
Niranjan Nair <niranjan.nair.email@gmail.com>
2024-07-25 21:19:39 -0400

    Restart

--- src/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef common_h
-#define common_h
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-
-#endif
--- src/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef debug_h
-#define debug_h
-
-#include "common.h"
-#include "bitboard.h"
-
-void print_bitboard(uint64_t bitboard);
-
-#endif
