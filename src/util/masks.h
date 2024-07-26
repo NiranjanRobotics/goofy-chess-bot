bdc91ceabdb9e75bb859c5d7512484c08acfbd80
Niranjan Nair <niranjan.nair.email@gmail.com>
2024-07-26 00:03:07 -0400

    redid enum to fit compilation, made functions more readable by adding newline for type

--- src/util/masks.h
+++ src/util/masks.h
@@ -4,29 +4,9 @@
 #include "common.h"
 #include "types.h"
 
-enum {
-    LIGHT_SQUARES = 0xAA55AA55AA55AA55ull,
-    DARK_SQUARES = ~LIGHT_SQUARES,
-
-    RANK_1 = 0x00000000000000FFull,
-    RANK_2 = RANK_1 << 8ull,
-    RANK_3 = RANK_1 << 16ull,
-    RANK_4 = RANK_1 << 24ull,
-    RANK_5 = RANK_1 << 32ull,
-    RANK_6 = RANK_1 << 40ull,
-    RANK_7 = RANK_1 << 48ull,
-    RANK_8 = RANK_1 << 56ull,
-
-    FILE_A = 0x8080808080808080ull,
-    FILE_B = 0x4040404040404040ull,
-    FILE_C = 0x2020202020202020ull,
-    FILE_D = 0x1010101010101010ull,
-    FILE_E = 0x0808080808080808ull,
-    FILE_F = 0x0404040404040404ull,
-    FILE_G = 0x0202020202020202ull,
-    FILE_H = 0x0101010101010101ull,
-
-    ALL_SQUARES = LIGHT_SQUARES & DARK_SQUARES,
-};
+extern const Bitboard
+    LIGHT_SQUARES, DARK_SQUARES, ALL_SQUARES,
+    RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8,
+    FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H;
 
 #endif
