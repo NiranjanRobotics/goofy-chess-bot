bcae237eb343ccfa16650dbb7ce528a9d2e7d078
Niranjan Nair <niranjan.nair.email@gmail.com>
2024-07-27 11:59:08 -0400

    fixed style

--- src/evaluation.c
+++ src/evaluation.c
@@ -1,1 +1,7 @@
 #include "evaluation.h"
+
+int
+evaluate_material(const Board *const board)
+{
+    return 0;
+}
--- src/evaluation.h
+++ src/evaluation.h
@@ -7,10 +7,19 @@
 #include "board.h"
 #include "move.h"
 
-int evaluate(const Board *const board);
-int evaluate_material(const Board *const board);
-int evaluate_pawns(const Board *const board);
-int evaluate_mobility(const Board *const board); // TODO: Add this after adding legal move generation
-int evaluate_king(const Board *const board);
+int
+evaluate(const Board *const board);
+
+int
+evaluate_material(const Board *const board);
+
+int
+evaluate_pawns(const Board *const board);
+
+int
+evaluate_mobility(const Board *const board); // TODO: Add this after adding legal move generation
+
+int
+evaluate_king(const Board *const board);
 
 #endif
