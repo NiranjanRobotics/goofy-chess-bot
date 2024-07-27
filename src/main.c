fef3c880de7ba197c91fdc2caabaa69dd9574fd7
2e Asteroid <two-e-asteroid@outlook.com>
2024-07-27 08:20:19 -0400

    Style guide; also added origin of safe_malloc

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include "debug.h"
 
 // Code that is actually run, currently used for debugging purposes
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-    Board *board = safe_malloc(sizeof(Board));
+    Board *board = safe_malloc(sizeof(Board)); // from util.h
     init_board(board, STARTING_POSITION, STARTING_STATE);
     print_board_info(board);
     free_board(board);
